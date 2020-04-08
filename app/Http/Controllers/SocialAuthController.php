1ab865e7c5f6184c703a9657d8e0e6e55adecbe4
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-04-08 08:21:30 +0700

    added google auth

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Socialite;
 use App\Services\SocialFacebookAccountService;
-// use App\Services\SocialGoogleAccountService;
+use App\Services\SocialGoogleAccountService;
 
 class SocialAuthController extends Controller
 {
+    private function createToken($user) {
+        return $user->createToken('Laravel Password Grant Client')->accessToken;
+    }
     /**
      * Create a redirect method to facebook api.
      *
@@ -19,6 +22,15 @@ class SocialAuthController extends Controller
         return Socialite::driver('facebook')->redirect();
     }
     /**
+     * Create a redirect method to google api.
+     *
+     * @return void
+     */
+    public function redirectGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    /**
      * Return a callback method from facebook api.
      *
      * @return callback URL from facebook
@@ -26,8 +38,18 @@ class SocialAuthController extends Controller
     public function callbackFacebook(SocialFacebookAccountService $service)
     {
         $user = $service->createOrGetUser(Socialite::driver('facebook')->stateless()->user());
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-        $response = ['token' => $token];
+        $response = ['token' => $this->createToken($user)];
+        return response($response, 200);
+    }
+    /**
+     * Return a callback method from facebook api.
+     *
+     * @return callback URL from facebook
+     */
+    public function callbackGoogle(SocialGoogleAccountService $service)
+    {
+        $user = $service->createOrGetUser(Socialite::driver('google')->stateless()->user());
+        $response = ['token' => $this->createToken($user)];
         return response($response, 200);
     }
 }
